a9683c24590aaa9107c77f170a7e3d05c39d554a
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-12-09 11:06:37 -0500

    duplication of code

---
+++ examples/part1/02_duplication_of_code/Sample.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Sample {  
+  public static int totalValues(List<Integer> numbers) {
+    int result = 0;
+    
+    for(int e : numbers) {
+      result += e;
+    }
+    
+    return result;
+  }
+
+  public static int totalEvenValues(List<Integer> numbers) {
+    int result = 0;
+    
+    for(int e : numbers) {
+      if(e % 2 == 0) result += e;
+    }
+    
+    return result;
+  }
+  
+  public static void main(String[] args) {
+    List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    
+    System.out.println(totalValues(numbers));
+    System.out.println(totalEvenValues(numbers));
+  }
+}
+//code duplication - violates the DRY principle
