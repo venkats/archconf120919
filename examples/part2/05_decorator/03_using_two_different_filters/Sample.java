70ceaea374e6bff47648f7a650665ae5010ec4ee
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-12-09 13:58:46 -0500

    using two different filters

---
+++ examples/part2/05_decorator/03_using_two_different_filters/Sample.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+import java.util.function.Function;
+
+class Camera {
+  private Function<Color, Color> filter = input -> input;
+  
+  public Camera() {}
+  public Camera(Function<Color, Color> aFilter) {
+    filter = aFilter;
+  }
+  
+  public Color snap(Color input) {
+    return filter.apply(input);
+  }
+}
+
+public class Sample { 
+  public static void printIt(Camera camera) {
+    System.out.println(camera.snap(new Color(125, 125, 125)));
+  }
+   
+  public static void main(String[] args) {
+    printIt(new Camera());
+    printIt(new Camera(Color::brighter));
+    printIt(new Camera(Color::darker));
+  }
+}
